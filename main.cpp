2a1b27ee1ed9de216897c2f7e1b8430c12cdc092
PlungedInCode <plungedincode@gmail.com>
2023-12-24 19:54:15 +0300

    Updated path

--- main.cpp
+++ main.cpp
@@ -1,20 +1,27 @@
 #include <string>
+// #include "UI/UIManager.hpp"
+#include "Logger.hpp"
 #include "DB/Utils.hpp"
 
 using namespace Utils;
 
 int main() {
-  
-  // std::string DBTEST_folderPath = "music/DBTEST";
-  // std::string Fulls_folderPath = "music/Fulls";
-  std::string Samples_folderPath = "../music/Samples";
-  // std::string SamplesTest_folderPath = "music/SamplesTEST";
 
-  DataBase db("../TESTDB.db");
+  Logger::SetLogLevel(LogLevel::kTrace);
+  Logger::DisableFileStamp();
+  Logger::SetStream(OutputStream::kBoth);
+  Logger::SetLogFile("20Songs.txt");
+  // std::string DBTEST_folderPath = "music/DBTEST";
+  // std::string Fulls_folderPath = "../music/Fulls";
+  std::string Samples_folderPath = "../music/Samples";
+  // std::string SamplesTest_folderPath = "../music/SamplesTEST";
+
+  DataBase db("20SongsCP.db");
 
   try {
-    // LoadFolder(db, folderPath);
+    // LoadFolder(db, Fulls_folderPath);
     TestFolder(db, Samples_folderPath);
+    // TestFolder(db, SamplesTest_folderPath);
   } catch (const std::exception& e) {
     std::cerr << "Error: " << e.what() << std::endl;
     return 1;
