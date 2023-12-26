56c7f03bb37a08741785f856bc932a2973be63e9
PlungedInCode <plungedincode@gmail.com>
2023-12-26 21:51:29 +0300

    main

--- main.cpp
+++ main.cpp
@@ -1,36 +1,17 @@
-#include <string>
-// #include "UI/UIManager.hpp"
+#include "UI/UIManager.hpp"
 #include "Logger.hpp"
-#include "DB/Utils.hpp"
 
-using namespace Utils;
+int main(int argc, char **argv) {
 
-int main() {
-
-  Logger::SetLogLevel(LogLevel::kTrace);
+  Logger::SetLogLevel(LogLevel::kInfo);
   Logger::DisableFileStamp();
-  Logger::SetStream(OutputStream::kBoth);
-  Logger::SetLogFile("20Songs.txt");
-  std::string DBTEST_folderPath = "../music/TestFulls";
-  std::string SamplesTest_folderPath = "../music/TestSamples";
-  std::string Fulls_folderPath = "../music/Fulls";
-  std::string Samples_folderPath = "../music/Samples";
 
-  // DataBase db("FLATTEDPEAKS.db");
-  // DataBase db("MINMAXFPEAKS.db");
-  DataBase db("FLATTEDPEAKSTEST.db");
-  // DataBase db("MINMAXFPEAKSTEST.db");
-
-  try {
-    // LoadFolder(db, Fulls_folderPath);
-    // TestFolder(db, Samples_folderPath);
-
-    LoadFolder(db, DBTEST_folderPath);
-    TestFolder(db, SamplesTest_folderPath);
-  } catch (const std::exception& e) {
-    LOG_ERROR("Error: ", e.what());
-    return 1;
+  if (argc == 2) {
+    // TODO: 
+    // 1. Loading folder
+    // 2. Testing folder
   }
 
-  return 0;
+  AudioIdentifier app;
+  app.Run();
 }
